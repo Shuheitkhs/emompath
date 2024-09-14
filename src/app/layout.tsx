5aee1344e0192e466d61ad5db4e4572eb68f9561
Stk <shuheitakahashi0822@gmail.com>
2024-09-14 23:34:36 +0900

    💄 フォントの導入

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,42 +1,5 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
-import "../styles/globals.css"; // publicフォルダ内のファイルは/から始める
-
-const kazesawa = localFont({
-  src: [
-    {
-      path: "../public/fonts/Kazesawa-Regular.woff", // 相対パスで指定
-      weight: "400",
-      style: "normal",
-    },
-    {
-      path: "../public/fonts/Kazesawa-Bold.woff",
-      weight: "700",
-      style: "normal",
-    },
-    {
-      path: "../public/fonts/Kazesawa-ExtraLight.woff",
-      weight: "200",
-      style: "normal",
-    },
-    {
-      path: "../public/fonts/Kazesawa-Extrabold.woff",
-      weight: "800",
-      style: "normal",
-    },
-    {
-      path: "../public/fonts/Kazesawa-Light.woff",
-      weight: "300",
-      style: "normal",
-    },
-    {
-      path: "../public/fonts/Kazesawa-Semibold.woff",
-      weight: "600",
-      style: "normal",
-    },
-  ],
-  variable: "--font-kazesawa",
-});
+import "../styles/globals.css";
 
 export const metadata: Metadata = {
   title: "EMOM Path",
@@ -51,15 +14,19 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${kazesawa.variable} antialiased`}>
+      <body className="font-en antialiased">
         <video
           autoPlay
           muted
           loop
           className="absolute inset-0 w-full h-full object-cover"
-        ></video>
-        <source src="/emompath.mp4" type="video/mp4" />
-        {children}
+        >
+          <source src="/emompath.mp4" type="video/mp4" />
+        </video>
+        <div className="absolute inset-0 bg-gray-600 bg-opacity-30"></div>
+
+        {/* コンテンツ */}
+        <div className="relative z-10">{children}</div>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,8 @@ export default function Home() {
   return (
     <div>
       <h1>Hello World</h1>
+      <p>フォントのテスト</p>
+      <p>exercise</p>
     </div>
   );
 }
