8380a5ec932d52e1a0c91076de5f283552ad8dd4
Stk <shuheitakahashi0822@gmail.com>
2024-09-16 22:21:45 +0900

    💄 ボタンデザインの確認

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,65 @@
-import Button from "@/components/Button";
+"use client";
+import AddIcon from "@mui/icons-material/Add";
+import RemoveIcon from "@mui/icons-material/Remove";
+import { Button } from "@/components/Button";
+import { Fab } from "@mui/material";
 import Image from "next/image";
 
 export default function Home() {
+  const Hello = () => {
+    alert("Hello");
+  };
+
   return (
     <div>
       <h1>Hello World</h1>
       <p>フォントのテスト</p>
       <p>exercise</p>
-      <Button children={"テスト"}></Button>
+      <Button size="extra-small" color="secondary" onClick={Hello}>
+        {" "}
+        <AddIcon />
+      </Button>
+      <Button size="extra-small" color="secondary" onClick={Hello}>
+        {" "}
+        <RemoveIcon />
+      </Button>
+      <Button
+        size="small"
+        color="secondary"
+        children={"テスト"}
+        onClick={Hello}
+      ></Button>
+      <Button
+        size="small"
+        color="secondary"
+        children={"テスト"}
+        onClick={Hello}
+      ></Button>
+      <Button
+        size="small"
+        color="secondary"
+        children={"テスト"}
+        onClick={Hello}
+      ></Button>
+      <Button
+        size="medium"
+        color="danger"
+        children={"Start!"}
+        onClick={Hello}
+      ></Button>
+      <Button
+        size="medium"
+        color="danger"
+        children={"Cancel"}
+        onClick={Hello}
+      ></Button>
+      <Button size="large" children={"テスト"} onClick={Hello}></Button>
+      <Button
+        size="medium"
+        color="danger"
+        children={"テスト"}
+        onClick={Hello}
+      ></Button>
     </div>
   );
 }
--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,37 +1,42 @@
-// ボタンを4種類、カラーを3種類で使い分け
+// ボタンを5種類、カラーを3種類で使い分け
 // small +-ボタン
 // medium 親要素45%
 // large　親要素90%
-// デフォルト値はauthフォルダ内で使うもの
+// デフォルト値はnormal
 type ButtonProps = {
-  size?: "small" | "medium" | "large" | "auth";
+  size?: "extra-small" | "small" | "medium" | "large" | "normal";
   color?: "primary" | "secondary" | "danger";
   onClick: () => void;
   children: React.ReactNode;
 };
 
-const Button: React.FC<ButtonProps> = ({
-  size = "auth",
+export const Button: React.FC<ButtonProps> = ({
+  size = "normal",
   color = "primary",
   onClick,
   children,
 }) => {
   let sizeClass = "";
   switch (size) {
+    case "extra-small":
+      sizeClass =
+        "py-2 px-2 text-xl text-text_dark font-bold w-auto m-1 hover:scale-105 transition-transform duration-100 active:scale-95"; // +-ボタンのみに使用
+      break;
     case "small":
-      sizeClass = "py-2 px-2 text-md w-auto"; // +-ボタンのみに使用
+      sizeClass =
+        "py-2 px-4 text-xl font-bold w-[30%] m-1 hover:scale-105 transition-transform duration-100 active:scale-95"; // 横幅30%
       break;
     case "medium":
-      sizeClass = "py-2 px-4 text-md w-[45%]"; // 横幅45%
+      sizeClass =
+        "py-2 px-4 text-2xl font-bold w-[45%] m-1 hover:scale-105 transition-transform duration-100 active:scale-95"; // 横幅45%
       break;
     case "large":
-      sizeClass = "py-3 px-6 text-lg w-[90%]"; // 横幅90%
-      break;
-    case "auth":
-      sizeClass = "py-2 px-4 text-md w-auto"; //auth系に使用
+      sizeClass =
+        "py-3 px-6 text-2xl font-bold w-[90%] m-1 hover:scale-105 transition-transform duration-100 active:scale-95"; // 横幅90%
       break;
     default:
-      sizeClass = "py-2 px-4 text-md w-auto";
+      sizeClass =
+        "py-2 px-4 text-xl font-bold w-auto m-1 hover:scale-105 transition-transform duration-100 active:scale-95"; // normalボタン用
   }
 
   // primaryは進行形アクション・secondaryは追加系アクション・dangerは削除・戻る系アクション
