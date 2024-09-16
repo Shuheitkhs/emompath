dbff766a3f7d05152aadd6268374e4c8c35fc1fd
Stk <shuheitakahashi0822@gmail.com>
2024-09-16 21:08:22 +0900

    💄 ボタンのスタイルの追加

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,9 +1,55 @@
-const Button: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+// ボタンを4種類、カラーを3種類で使い分け
+// small +-ボタン
+// medium 親要素45%
+// large　親要素90%
+// デフォルト値はauthフォルダ内で使うもの
+type ButtonProps = {
+  size?: "small" | "medium" | "large" | "auth";
+  color?: "primary" | "secondary" | "danger";
+  onClick: () => void;
+  children: React.ReactNode;
+};
+
+const Button: React.FC<ButtonProps> = ({
+  size = "auth",
+  color = "primary",
+  onClick,
+  children,
+}) => {
+  let sizeClass = "";
+  switch (size) {
+    case "small":
+      sizeClass = "py-2 px-2 text-md w-auto"; // +-ボタンのみに使用
+      break;
+    case "medium":
+      sizeClass = "py-2 px-4 text-md w-[45%]"; // 横幅45%
+      break;
+    case "large":
+      sizeClass = "py-3 px-6 text-lg w-[90%]"; // 横幅90%
+      break;
+    case "auth":
+      sizeClass = "py-2 px-4 text-md w-auto"; //auth系に使用
+      break;
+    default:
+      sizeClass = "py-2 px-4 text-md w-auto";
+  }
+
+  // primaryは進行形アクション・secondaryは追加系アクション・dangerは削除・戻る系アクション
+  let colorClass = "";
+  switch (color) {
+    case "secondary":
+      colorClass = "bg-secondary text-text"; //ネオンブルー
+      break;
+    case "danger":
+      colorClass = "bg-danger text-text"; //ネオングレー
+      break;
+    default:
+      colorClass = "bg-primary text-text"; //ネオンピンク
+  }
+
   return (
-    <button className="bg-gradient-to-r from-red-800 to-red-700 text-gray-300 font-semibold py-1 px-4 rounded-lg shadow-lg  transition-shadow duration-500 ease-in-out active:shadow-inner transform hover:scale-105">
+    <button className={`rounded ${sizeClass} ${colorClass}`} onClick={onClick}>
       {children}
     </button>
   );
 };
-
-export default Button;
