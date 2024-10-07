6df8f305bd224fc3e1e384d408e06f7f449faefd
Shogo Wada <aiz4338150@gmail.com>
2024-10-07 19:14:04 +0900

    middlewareの修正

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,6 @@
-import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
-import { createMiddlewareClient } from "@supabase/auth-helpers-nextjs";
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+import { createMiddlewareClient } from '@supabase/auth-helpers-nextjs';
 
 export async function middleware(req: NextRequest) {
   const res = NextResponse.next();
@@ -9,26 +9,19 @@ export async function middleware(req: NextRequest) {
   const supabase = createMiddlewareClient({ req, res });
 
   // 認証が必要なパスを定義
-  const protectedPaths = ["/emoms", "/auth/mypage"];
+  const protectedPaths = ['/emoms', '/auth/mypage'];
 
   // リクエストのパスが認証が必要な場合
   if (protectedPaths.some((path) => req.nextUrl.pathname.startsWith(path))) {
-    const accessToken = req.cookies.get("supabase-auth-token")?.value;
-
-    if (!accessToken) {
-      // 認証されていない場合は、サインインページにリダイレクト
-      return NextResponse.redirect(new URL("/auth/signin", req.url));
-    }
-
     // アクセストークンを使用してユーザー情報を取得
     const {
       data: { user },
       error,
-    } = await supabase.auth.getUser(accessToken);
+    } = await supabase.auth.getUser();
 
     if (error || !user) {
       // トークンが無効な場合は、サインインページにリダイレクト
-      return NextResponse.redirect(new URL("/auth/signin", req.url));
+      return NextResponse.redirect(new URL('/auth/signin', req.url));
     }
   }
 
