e45640b9e2d0bac46b9d1b302d1d96cc29a9023c
Stk <shuheitakahashi0822@gmail.com>
2024-10-20 19:21:56 +0900

    💄 コンプリートダイアログのスクロール表示実装

--- src/components/CompleteDialog.tsx
+++ src/components/CompleteDialog.tsx
@@ -87,25 +87,41 @@ const CompleteDialog: React.FC<CompleteDialogProps> = ({ emom, onUpdate }) => {
 
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
-      <div className="bg-danger p-5 rounded shadow-lg">
+      <div className="bg-danger p-5 rounded shadow-lg flex flex-col max-h-[90vh] w-full max-w-2xl">
         <h3 className="text-primary font-bold text-5xl mb-4">
           Congratulations!!
         </h3>
         <p className="text-xl font-bold mb-4">
           Now get ready for your next training session!
         </p>
-        {workoutPlans.map((plan, index) => (
-          <AlertDialog
-            key={index}
-            trigger={
-              <div className="bg-black rounded-lg cursor-pointer text-2xl shadow-xl hover:scale-105 transition-transform duration-100 active:scale-95 p-4 mb-4">
-                <div>{icons[index]}</div>
-                <div className="text-3xl">
-                  Volume{" "}
-                  <span className="text-primary">{plan.volumeIncrease}</span> up
+        <div className="flex-grow overflow-y-auto">
+          {workoutPlans.map((plan, index) => (
+            <AlertDialog
+              key={index}
+              trigger={
+                <div className="bg-black rounded-lg cursor-pointer text-2xl shadow-xl hover:scale-105 transition-transform duration-100 active:scale-95 p-4 mb-4">
+                  <div>{icons[index]}</div>
+                  <div className="text-3xl">
+                    Volume{" "}
+                    <span className="text-primary">{plan.volumeIncrease}</span>{" "}
+                    up
+                  </div>
+                  <div className="grid grid-cols-2 gap-5 text-2xl py-2 my-2">
+                    <div className="">{emom.name}</div>
+                    <div>{plan.sets} sets</div>
+                    {plan.exercises.map((exercise) => (
+                      <React.Fragment key={exercise.id}>
+                        <div>{exercise.name}</div>
+                        <div>{exercise.reps} reps</div>
+                      </React.Fragment>
+                    ))}
+                  </div>
                 </div>
-                <div className="grid grid-cols-2 gap-5 text-2xl py-2 my-2">
-                  <div className="">{emom.name}</div>
+              }
+              title={`Update with ${plan.volumeIncrease} increase?`}
+              content={
+                <div className="grid grid-cols-2 gap-5 text-2xl text-center">
+                  <div>{emom.name}</div>
                   <div>{plan.sets} sets</div>
                   {plan.exercises.map((exercise) => (
                     <React.Fragment key={exercise.id}>
@@ -114,27 +130,15 @@ const CompleteDialog: React.FC<CompleteDialogProps> = ({ emom, onUpdate }) => {
                     </React.Fragment>
                   ))}
                 </div>
-              </div>
-            }
-            title={`Update with ${plan.volumeIncrease} increase?`}
-            content={
-              <div className="grid grid-cols-2 gap-5 text-2xl text-center">
-                <div>{emom.name}</div>
-                <div>{plan.sets} sets</div>
-                {plan.exercises.map((exercise) => (
-                  <React.Fragment key={exercise.id}>
-                    <div>{exercise.name}</div>
-                    <div>{exercise.reps} reps</div>
-                  </React.Fragment>
-                ))}
-              </div>
-            }
-            agreeText="Yes"
-            disagreeText="No"
-            onAgree={() => handleAgree(plan)}
-            onDisagree={handleDisagree}
-          />
-        ))}
+              }
+              agreeText="Yes"
+              disagreeText="No"
+              onAgree={() => handleAgree(plan)}
+              onDisagree={handleDisagree}
+            />
+          ))}
+        </div>
+
         {showConfirmation && <UpdateDialog onClose={handleCloseConfirmation} />}
       </div>
     </div>
