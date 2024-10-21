0613ba0872db091856f2c9449d15b20bd4c5957f
Stk <shuheitakahashi0822@gmail.com>
2024-10-22 02:02:25 +0900

    🚧 google認証のテスト

--- src/lib/supabaseServerClient.ts
+++ src/lib/supabaseServerClient.ts
@@ -1,17 +1,16 @@
-// // lib/supabaseServerClient.ts
-// import { createClient } from "@supabase/supabase-js";
+import { createClient } from "@supabase/supabase-js";
 
-// const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-// const supabaseServiceRoleKey = process.env.SUPABASE_SERVICE_ROLE_KEY!;
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseServiceRoleKey = process.env.SUPABASE_SERVICE_ROLE_KEY!;
 
-// export const supabaseServerClient = createClient(
-//   supabaseUrl,
-//   supabaseServiceRoleKey,
-//   {
-//     auth: {
-//       autoRefreshToken: false,
-//       persistSession: false,
-//       detectSessionInUrl: false,
-//     },
-//   }
-// );
+export const supabaseServerClient = createClient(
+  supabaseUrl,
+  supabaseServiceRoleKey,
+  {
+    auth: {
+      autoRefreshToken: false,
+      persistSession: false,
+      detectSessionInUrl: false,
+    },
+  }
+);
