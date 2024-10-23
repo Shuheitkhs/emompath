4cad4e3943dfce625e11e3685909f8f071e0b017
Stk <shuheitakahashi0822@gmail.com>
2024-10-23 19:31:10 +0900

    💄 スタイリング微調整

--- src/components/molecules/Counter.tsx
+++ src/components/molecules/Counter.tsx
@@ -15,13 +15,23 @@ const Counter: React.FC<CounterProps> = ({ title, number, plus1, minus1 }) => {
   return (
     <div className="w-full px-[5%] py-3">
       <div className="flex justify-between">
-        <Button size="extra-small" color="secondary" onClick={minus1}>
+        <Button
+          size="extra-small"
+          color="secondary"
+          onClick={minus1}
+          className="text-2xl"
+        >
           -
         </Button>
         <p className="text-2xl font-bold">
           {`${title}`} <span className="text-3xl">{`${number}`}</span>
         </p>
-        <Button size="extra-small" color="secondary" onClick={plus1}>
+        <Button
+          size="extra-small"
+          color="secondary"
+          onClick={plus1}
+          className="text-2xl"
+        >
           +
         </Button>
       </div>
