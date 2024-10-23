c9f5f7f5293c01c3a07c6c9346e18f9d3ddb4c68
Stk <shuheitakahashi0822@gmail.com>
2024-10-23 13:40:06 +0900

    💄 ボタンのレイアウト調整

--- src/components/atoms/Button.tsx
+++ src/components/atoms/Button.tsx
@@ -30,27 +30,21 @@ const Button: React.FC<ButtonProps> = ({
   let sizeClass = "";
   switch (size) {
     case "extra-small":
-      sizeClass =
-        "py-2 px-2 text-xl text-text_dark font-bold w-[10%]  hover:scale-105 transition-transform duration-100 active:scale-95"; // +-ボタンのみに使用
+      sizeClass = "py-2 px-2 text-xl text-text_dark font-bold w-[10%]"; // +-ボタンのみに使用
       break;
     case "small":
-      sizeClass =
-        "py-2 px-4 text-xl font-bold w-[30%]  hover:scale-105 transition-transform duration-100 active:scale-95"; // 横幅30%
+      sizeClass = "py-2 px-4 text-sm font-bold w-[30%] sm:text-xl"; // 横幅30%
       break;
     case "medium":
-      sizeClass =
-        "py-2 px-4 text-2xl font-bold w-[45%]  hover:scale-105 transition-transform duration-100 active:scale-95"; // 横幅45%
+      sizeClass = "py-2 px-4 text-l font-bold w-[45%]  sm:text-2xl"; // 横幅45%
       break;
     case "large":
-      sizeClass =
-        "py-3 px-6 text-2xl font-bold w-[90%]  hover:scale-110 transition-transform duration-100 active:scale-95"; // 横幅90%
+      sizeClass = "py-3 px-6 text-l font-bold w-[90%]  sm:text-2xl"; // 横幅90%
       break;
     case "full":
-      sizeClass =
-        "py-3 px-6 text-2xl font-bold w-[110%] h-[100%]  hover:scale-105 transition-transform duration-100 active:scale-95";
+      sizeClass = "py-3 px-6 text-l font-bold w-[110%] h-[100%]  sm:text-2xl";
     default:
-      sizeClass =
-        "py-2 px-4 text-xl font-bold w-auto  hover:scale-105 transition-transform duration-100 active:scale-95"; // normalボタン用
+      sizeClass = "py-2 px-4 text-l font-bold w-auto sm:text-2xl "; // normalボタン用
   }
 
   // primaryは進行形アクション・secondaryは追加系アクション・dangerは削除・戻る系アクション
@@ -68,7 +62,7 @@ const Button: React.FC<ButtonProps> = ({
 
   return (
     <button
-      className={`text-center rounded 
+      className={`text-center rounded hover:scale-105 transition-transform duration-100 active:scale-95
          ${sizeClass} ${colorClass} ${className}`}
       onClick={onClick}
     >
